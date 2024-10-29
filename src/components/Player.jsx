fa1e256fb864ec93519c69a012afb4fb1704f9ff
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-29 12:18:03 +0000

    created functionality for the set name input bar

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [hasClicked, setHasClicked] = useState(false);
+  const [name, setName] = useState("unkown entity");
+
+  function handleClick() {
+    setHasClicked((prevValue) => !prevValue);
+  }
+
+  function handleChange(event) {
+    setHasClicked(false);
+    setName(event.target.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {hasClicked ? name : "unkown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={name} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
